fc8b88e3389c0b5ed9c112b2310c2b5682048049
lazybones <125787989@qq.com>
2019-05-14 17:14:56 +0800

    纹理基础篇：纹理的重复与纹理的回环以及纹理偏移

--- js/learn9.js
+++ js/learn9.js
@@ -7,112 +7,79 @@ window.onload = function () {
 var renderer, camera, scene;
 var mesh;
 var texture = null;
+var stats;
 
-
-
-
-
-
-function initThree(){
-    width = window.innerWidth;
-    height = window.innerHeight;
-    renderer = new THREE.WebGLRenderer({
-        antialiias: true
-    })
-
-    renderer.setSize(width, height);
+function init() {
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    renderer.setClearColor(0x000000, 1.0);
 
     stats = new Stats();
     stats.domElement.style.position = "absolute";
     stats.domElement.style.left = "0px";
     stats.domElement.style.top = "0px";
     document.body.appendChild(stats.domElement);
-}
 
-var camera;
-function initCamera(){
-    camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
-    camera.position.set(0,0,3);
-}
-
-var scene;
-function initScene(){
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.z = 400;
     scene = new THREE.Scene();
 
-    //添加一个辅助坐标轴
-    // var axes = new THREE.AxisHelper(2000);
-    // scene.add(axes);
-}
-
-var light;
-function initLight(){
-    light = new THREE.DirectionalLight(0xFFFFFF,1); 
-    light.position.set(0, 0, 10);
-    scene.add(light);
-}
-
-var mesh;
-var textureName = 1;
-function initObject(){
-    //创建一个立方体的几何体
-    var geometry = new THREE.SphereBufferGeometry(0.25, 60, 60);
-
-    //获取纹理
+    var geometry = new THREE.PlaneGeometry(500,300,1,1);  //平面模型
+    var material;
     var loader = new THREE.TextureLoader();
-    var texture = loader.load("../texture/" + textureName + '.png', function(){
-        var material = new THREE.MeshPhongMaterial({map:texture});
-        mesh = new THREE.Mesh(geometry, material);
+    loader.load('../texture/5.png', function(text){
+        texture = text;
+        material = new THREE.MeshBasicMaterial({
+            map: texture
+        });
+        var mesh = new THREE.Mesh(geometry, material);
         scene.add(mesh);
+    },function(xhr){
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+    },function(){
+        console.log( 'An error happened' );
     });
+
+    createUI();
+    window.addEventListener("resize", onWindowResize, false);   
 }
 
-var isChange = false;
-var isStop = false;
-function animation(){
+var param;
+function createUI(){
+    var parameter = function(){
+        this.repeat = 1;
+        this.wrap = 1;
+    }
+    param = new parameter();
+    var gui = new dat.GUI();
+    gui.add(param, "repeat", 1, 5).name("纹理重复");
+    gui.add(param, "wrap", 1, 3).name("纹理环绕").step(1);
+}
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate(){
     stats.begin();
-
-    if(!isStop && mesh){
-        mesh.rotation.y += 0.01;
-    }
-
-    if(isChange){
-        isChange = !isChange;
-        
-        if(textureName < 4){
-            textureName++;
-        }else{
-            textureName = 1;
-        }
-        
-        let image = new Image()
-        image.src ="../texture/" + textureName + '.png';
-        image.onload = function () {
-            mesh.material.map.image = image;
-            mesh.material.map.needsUpdate = true;
-        }
-    }
-
+    change();
+    requestAnimationFrame(animate);
     renderer.render(scene, camera);
-
-    requestAnimationFrame(animation);
-
     stats.end();
 }
 
-var timer=null;
-function changeMaterial(){
-    document.body.onclick = function () {
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-            isChange = !isChange;
-        },250)
-        
-    }
-
-    document.body.ondblclick = function () {
-        clearTimeout(timer);
-        isStop = !isStop;
+function change(){
+    if(texture != null){
+        texture.repeat.x = texture.repeat.y = param.repeat;
+        if(param.wrap == 1){
+            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        }else if(param.wrap == 2){
+            texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
+        }else if(param.wrap == 3){
+            texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+        }
+        texture.needsUpdate = true;
     }
 }
